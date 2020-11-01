284a95f1413bdc42c931b04cc8bdd8b8abc58049
cvmartin <cvarelamail@gmail.com>
2020-11-01 12:52:16 +0100

    update app.R. Now it deploys well

--- app.R
+++ app.R
@@ -1,2 +1,6 @@
- pkgload::load_all(export_all = FALSE, helpers = FALSE, attach_testthat = FALSE)
+# ShinyApp() deployment: use rsconnect::deployApp()
+# if using `renv` do NOT upload `.Rprofile`
+
+pkgload::load_all(export_all = FALSE, helpers = FALSE, attach_testthat = FALSE)
 eflows.site::run_app()
+
